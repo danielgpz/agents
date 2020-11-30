04a8b86bcebc1f574ed1900aebbe246caf5796ce
CoNKeR <d.garcia@estudiantes.matcom.uh.cu>
2020-11-30 17:05:20 -0500

    Add report

--- test.py
+++ test.py
@@ -1,23 +1,36 @@
 from sim import KinderGarden, Baby, Kangaroo, Cleaner
-import sys
 
-n, m, dr, ob, bs, t = (int(arg) for arg in sys.argv[1:])
+latex_sec = '\\subsection{Tablero de $%d \\times %d$, %d\\%% de basura, %d\\%% de obst\\\'aculos, %d ni\\~nos y t=%d}'
+latex_sim = '''
+\\textbf{%s}:
+\\begin{itemize}
+\\item Victorias: %d/30
+\\item Despedido: %d/30
+\\item Suciedad: %.2f\\%%
+\\end{itemize}
+'''
 
-won1, fired1, dirt_p1 = 0, 0, []
-won2, fired2, dirt_p2 = 0, 0, []
-for _ in range(30):
-    kangaroo, cleaner = Kangaroo(), Cleaner()
-    kg1 = KinderGarden(n, m, dr, ob, [Baby() for _ in range(bs)], [kangaroo], t)
-    kg2 = KinderGarden(n, m, dr, ob, [Baby() for _ in range(bs)], [cleaner], t)
+tests = [(10, 10, 5, ob, bs, t) for ob in range(0, 75, 25) for bs in range(4, 10, 3) for t in range(5, 15, 5)]
 
-    w, dirt_p = kg1.simulate(verbose=False)
-    won1 += w == 1
-    fired1 += w == -1
-    dirt_p1.append(dirt_p)
-    w, dirt_p = kg2.simulate(verbose=False)
-    won2 += w == 1
-    fired2 += w == -1
-    dirt_p2.append(dirt_p)
+for test in tests:
+    n, m, dr, ob, bs, t = test
 
-print(f'Kangaroo\n\tWon: \t{won1}\n\tFired: \t{fired1}\n\tDirt%: \t{round(100 * sum(dirt_p1)/len(dirt_p1), 2)}%')
-print(f'Cleaner\n\tWon: \t{won2}\n\tFired: \t{fired2}\n\tDirt%: \t{round(100 * sum(dirt_p2)/len(dirt_p2), 2)}%')    
+    won1, fired1, dirt_p1 = 0, 0, []
+    won2, fired2, dirt_p2 = 0, 0, []
+    for _ in range(30):
+        kangaroo, cleaner = Kangaroo(), Cleaner()
+        kg1 = KinderGarden(n, m, dr, ob, [Baby() for _ in range(bs)], [kangaroo], t)
+        kg2 = KinderGarden(n, m, dr, ob, [Baby() for _ in range(bs)], [cleaner], t)
+
+        w, dirt_p = kg1.simulate(verbose=False)
+        won1 += w == 1
+        fired1 += w == -1
+        dirt_p1.append(dirt_p)
+        w, dirt_p = kg2.simulate(verbose=False)
+        won2 += w == 1
+        fired2 += w == -1
+        dirt_p2.append(dirt_p)
+
+    print(latex_sec % (n, m, dr, ob, bs, t))
+    print(latex_sim % ('Kangaroo', won1, fired1, 100 * sum(dirt_p1)/len(dirt_p1)) )
+    print(latex_sim % ('Cleaner', won2, fired2, 100 * sum(dirt_p2)/len(dirt_p2)) )
