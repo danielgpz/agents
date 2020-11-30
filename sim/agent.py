750959bec251c1be49d155d141984f719363725c
CoNKeR <d.garcia@estudiantes.matcom.uh.cu>
2020-11-29 23:17:18 -0500

    Add two types of reactive agentes

--- sim/agent.py
+++ sim/agent.py
@@ -1,6 +1,5 @@
 from .environtment import Cell, Action, dx, dy
 from random import choice
-
 class Agent:
     def __init__(self, x=0, y=0):
         self.x, self.y = x, y
@@ -13,8 +12,6 @@ class Agent:
 
     def action(self, percetion):
         return NotImplementedError()
-
-
 class Baby(Agent):
     def action(self, perception):
         actions = [Action.Stay]
@@ -49,6 +46,35 @@ class Robot(Agent):
         self.baby = None
         return baby
 
+    def get_direction_to(self, env, cell, doble_steep=False):
+        n, m = env.n, env.m
+        start, it = (self.x, self.y), 0
+        queue = [start]
+        parent = {}
+        path = []
+        while it < len(queue):
+            x, y = queue[it]
+            it += 1
+            for d1, d2 in zip(dx[:4], dy[:4]):
+                pos = (i, j) = (x + d1, y + d2)
+                if (pos not in parent) and (0 <= i < n) and (0 <= j < m):
+                    if (env.cells[i][j] <= Cell.Playpen):
+                        queue.append(pos)
+                        parent[pos] = (x, y)
+                    if env.cells[i][j] == cell:
+                        path = [pos, (x, y)]
+                        while path[-1] != start: path.append(parent[path[-1]])
+                        (i, j), (x, y) = path[-2:]
+                        dif = (i - x, j - y)
+                        act, = (t for t, d in enumerate(zip(dx[:4], dy[:4])) if d == dif)
+                        if doble_steep and len(path) > 2:
+                            ii, jj = path[-3]
+                            dif2 = (ii - x, jj - y)
+                            act2, = (t for t, d in enumerate(zip(dx[act::4], dy[act::4])) if d == dif2)
+                            return Action(act + 4 * act2)
+                        return Action(act)
+            
+
     def __str__(self):
         return f'R[{self.x},{self.y}]' + ('(B)' if self.baby else '')
 
@@ -60,68 +86,43 @@ class StaticRobot(Robot):
     def action(self, perception):
         return Action.Stay
 
-class ReactiveRobot(Robot):
-    def find_path(self, env, cell):
-        n, m = env.n, env.m
-        x, y = self.x, self.y
-        queue = [(x, y)]
-        parent = {(x, y): (x, y)}
-        for it in range(n * m):
-            try:
-                x, y = queue[it]
-            except IndexError:
-                break
-
-            for d1, d2 in zip(dx[:4], dy[:4]):
-                pos = (i, j) = (x + d1, y + d2)
-                if (pos not in parent) and (0 <= i < n) and (0 <= j < m):
-                    if (env.cells[i][j] <= Cell.Playpen):
-                        queue.append(pos)
-                        parent[pos] = (x, y)
-                    if env.cells[i][j] == cell:
-                        path = [pos, (x, y)]
-                        while True:
-                            last = path[-1]
-                            plast = parent[last]
-                            if last != plast:
-                                path.append(plast)
-                            else:
-                                return path
-
+class Kangaroo(Robot):
     def action(self, perception):
         x, y = self.x, self.y
         cell = perception.cells[x][y]
 
         if cell == Cell.Dirt: return Action.Clean
 
-        if not self.baby:
-            path = self.find_path(perception, Cell.Baby)
-            if path:
-                i, j = path[-2]
-                dif = (i - x, j - y)
-                act, = (t for t, d in enumerate(zip(dx, dy)) if d == dif)
-                return Action(act)
+        if self.baby:
+            if cell == Cell.Playpen: return Action.Drop
+            direct = self.get_direction_to(perception, Cell.Playpen, doble_steep=True)
+            if direct: return direct
+        else:
+            direct = self.get_direction_to(perception, Cell.Baby)
+            if direct: return direct
+            
+        direct = self.get_direction_to(perception, Cell.Dirt)
+        if direct: return direct
+        
+        return Action.Stay
+
+class Cleaner(Robot):
+    def action(self, perception):
+        x, y = self.x, self.y
+        cell = perception.cells[x][y]
+
+        if cell == Cell.Dirt: return Action.Clean
 
         if self.baby:
             if cell == Cell.Playpen: return Action.Drop
-            path = self.find_path(perception, Cell.Playpen)
-            if path:
-                i, j = path[-2]
-                dif = (i - x, j - y)
-                act, = (t for t, d in enumerate(zip(dx, dy)) if d == dif)
-                try:
-                    ii, jj = path[-3]
-                    dif2 = (ii - x, jj - y)
-                    act2, = (t for t, d in enumerate(zip(dx[act::4], dy[act::4])) if d == dif2)
-                    return Action(act + 4 * act2)
-                except IndexError:
-                    return Action(act)
-
-        path = self.find_path(perception, Cell.Dirt)
-        if path:
-            i, j = path[-2]
-            dif = (i - x, j - y)
-            act, = (t for t, d in enumerate(zip(dx, dy)) if d == dif)
-            return Action(act)
-
+            direct = self.get_direction_to(perception, Cell.Playpen, doble_steep=True)
+            if direct: return direct
+        else:
+            direct = self.get_direction_to(perception, Cell.Dirt)
+            if direct: return direct
+        
+            direct = self.get_direction_to(perception, Cell.Baby)
+            if direct: return direct
+            
+        
         return Action.Stay
